62809378dbf2b1c8c9b1774546eb1f27e4070e7f
Benjamin <benjamin.sokolov19@gmail.com>
2024-06-29 16:59:18 +0300

    add: modal form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,11 +3,28 @@ import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+
+// export type ArticleParamsFormProps = {
+// 	setAppState: (value: ArticleStateType) => void;
+// };
+
 export const ArticleParamsForm = () => {
+	const asideRef = useRef<HTMLElement | null>(null);
+
+	const [formState, setFormState] = useState(false);
+
+	const toggleForm = () => {
+		setFormState(!formState);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isActive={formState} onClick={toggleForm} />
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, formState && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
