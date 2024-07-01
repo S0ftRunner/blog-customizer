0ea10025d6e963cf04887322a4b20b76bcf96a73
Benjamin <benjamin.sokolov19@gmail.com>
2024-07-01 11:15:04 +0300

    add: components in form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,31 +1,78 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Text } from '../text';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
 
 import styles from './ArticleParamsForm.module.scss';
 
 import { useRef, useState } from 'react';
 import clsx from 'clsx';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 
 // export type ArticleParamsFormProps = {
 // 	setAppState: (value: ArticleStateType) => void;
 // };
 
 export const ArticleParamsForm = () => {
+	// const {setAppState} = props;
+
 	const asideRef = useRef<HTMLElement | null>(null);
 
-	const [formState, setFormState] = useState(false);
+	const [isOpenForm, setToggleForm] = useState(false);
+
+	const [formState] = useState<ArticleStateType>(defaultArticleState);
 
 	const toggleForm = () => {
-		setFormState(!formState);
+		setToggleForm(!isOpenForm);
 	};
 
 	return (
 		<>
-			<ArrowButton isActive={formState} onClick={toggleForm} />
+			<ArrowButton isActive={isOpenForm} onClick={toggleForm} />
+			<div
+				onClick={() => setToggleForm(false)}
+				className={clsx(
+					styles.overlay,
+					isOpenForm && styles.overlay_open
+				)}></div>{' '}
+			{/*Данное решение нужно, чтобы модалка закрывалась при клике вне элемента*/}
 			<aside
 				ref={asideRef}
-				className={clsx(styles.container, formState && styles.container_open)}>
+				className={clsx(styles.container, isOpenForm && styles.container_open)}>
 				<form className={styles.form}>
+					<Text uppercase={true} size={31} weight={800}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}></Select>
+					<RadioGroup
+						name='шрифт'
+						title='размер шрифта'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}></RadioGroup>
+					<Select
+						title='цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}></Select>
+					<Select
+						title='цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}></Select>
+					<Select
+						title='ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
